c864d6327996e7cc38c24a43236976234138c84f
Henrik Forstén <henrik.forsten@gmail.com>
2014-06-09 18:36:07 +0300

    Fix bug with 16-bit wav files
    They were interpreted as 24-bit making the division with maximum value overflow

--- wavtospice.py
+++ wavtospice.py
@@ -14,19 +14,22 @@ def read_wav(filename):
     print 'Channels:',nchannels
     for j in range(nframes):
         waveData = waveFile.readframes(1)
+        #Take only the first channel
+        waveData = waveData[:sampwidth]
         if sampwidth==1:
             temp = struct.unpack_from("<b", waveData)
         elif sampwidth==2:
             temp = struct.unpack_from("<h", waveData)
         elif sampwidth==3:
-            temp = struct.unpack_from("<i", '\x00'+waveData)
+            temp = struct.unpack_from("<i", waveData+'\x00')
+        elif sampwidth==4:
+            temp = struct.unpack_from("<i", waveData)
         else:
             raise ValueError('Unsupported sample width')
-        data.append((t*timestep,float(temp[0])/(2**(8*sampwidth-1)-1)))
+        data.append((t*timestep,float(temp[0])/(2**(8*sampwidth)-1)))
         t += 1
     vrms = sqrt(1.0/(len(data[1]))*sum(i**2 for i in data[1]))
     print 'RMS voltage:',vrms
-    #data[1] = [(i/vrms)*0.025 for i in data[1]]
     return data
 
 

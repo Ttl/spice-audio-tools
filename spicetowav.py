29a80c77bef0f1d25545b73da94efb018fa29d5e
Ildus K <i.kurbangaliev@gmail.com>
2017-05-07 00:58:36 +0300

    Adapt code to python 3, refactor and clean the code

--- spicetowav.py
+++ spicetowav.py
@@ -1,73 +1,74 @@
-import sys, wave, struct
+#!/usr/bin/env python3
+#coding: utf-8
+
+import argparse
+import re
+import struct
+import sys
+import wave
+
 SAMPLING_RATE = 44100
 
-def parse_output(output):
-    value={}
-    output=output.split('\n')
-    index=1
-    current = ()
-    for line in xrange(len(output)):
-        temp=output[line].replace(',','').split()
-        if len(temp)>0:
-            if temp[0]=='Index':
-                if line+2<len(output):
-                    temp2=output[line+2].replace(',','').split()
-                    if float(temp2[0])<index:
-                        current = temp[2]
-                        value[temp[2]]=([],[])
-                        index=0
 
-        if len(temp)>2 and current!=():
-            try:
-                float(temp[1]),float(temp[2])
-            except:
-                continue
-            index+=1
-            value[current][0].append(float(temp[1]))
-            value[current][1].append(float(temp[2]))
-    return value
+def parse_output(spice_output):
+	''' parses ngspice output. use `wrdata <filename> v(<pin>)` command
+		to generate right output
+	'''
 
-def lin_interp(x0,x1,y0,y1,x):
+	times = []
+	voltages = []
+
+	with open(spice_output, 'r') as output:
+		for line in output:
+			if line:
+				row = re.split('\s+', line.strip())
+				times.append(float(row[0]))
+				voltages.append(float(row[1]))
+
+	return times, voltages
+
+
+def lin_interp(x0, x1, y0, y1, x):
     x0 = float(x0)
     x1 = float(x1)
     y0 = float(y0)
     y1 = float(y1)
     x = float(x)
-    return y0+(x-x0)*(y1-y0)/(x1-x0)
+    return y0 + (x - x0) * (y1 - y0) / (x1 - x0)
 
-def write_wav(data,filename):
-    w = wave.open(filename,'w')
-    k = data[data.keys()[0]]
-    w.setparams((1, 2, SAMPLING_RATE, 0, 'NONE', 'not compressed'))
-    m = max(max(k[1]),-min(k[1]))
-    if clipping==False:
-        vrange = m
-    values = []
-    t = 0.0
-    step = 1.0/SAMPLING_RATE
 
-    for i in xrange(len(k[1])-1):
-        while k[0][i]<=t*step<k[0][i+1]:
-            sample = lin_interp(k[0][i],k[0][i+1],k[1][i],k[1][i+1],t*step)/vrange
-            sample = 1 if sample >1 else sample
-            sample = -1 if sample <-1 else sample
-            d = struct.pack('<h',int(32767*sample))
-            values.append(d)
-            t += 1
+def write_wav(times, voltages, filename, clipping):
+	with wave.open(filename, 'w') as w:
+		w.setparams((1, 2, SAMPLING_RATE, 0, 'NONE', 'not compressed'))
+		m = max(max(voltages), -min(voltages))
+		vrange = clipping if clipping else m
 
-    w.writeframes(''.join(values))
-    w.close()
+		values = bytes([])
+		t = 0.0
+		step = 1.0 / SAMPLING_RATE
+
+		for i in range(len(voltages)-1):
+			while times[i] <= t * step < times[i+1]:
+				sample = lin_interp(times[i], times[i+1],
+					voltages[i], voltages[i+1], t*step) / vrange
+				sample = 1 if sample >1 else sample
+				sample = -1 if sample <-1 else sample
+				d = struct.pack('<h',int(32767 * sample))
+				values += d
+				t += 1
+
+		w.writeframes(values)
 
 
 if __name__ == "__main__":
-    if len(sys.argv)<=2:
-        print """Usage: spicetowav <file> <output> [clipping voltage, m = no clipping]
-        Converts spice transient output to wav format."""
-        exit()
-    lines = ''.join(open(sys.argv[1]).readlines())
-    clipping = False
-    if len(sys.argv)>3:
-        if sys.argv[3] != 'm':
-            vrange = float(sys.argv[3])
-            clipping = True
-    write_wav(parse_output(lines),sys.argv[2])
+	parser = argparse.ArgumentParser()
+	parser.add_argument("spice_output",
+		help="Specify file with spice output")
+	parser.add_argument("wav_file",
+		help="Specify filename for wav data")
+	parser.add_argument("--clipping", dest='clipping',
+			default=0, type=float)
+	args = parser.parse_args()
+
+	times, voltages = parse_output(args.spice_output)
+	write_wav(times, voltages, args.wav_file, args.clipping)
--- wavtospice.py
+++ wavtospice.py
@@ -1,45 +1,58 @@
-#Converts wav file to ngspice filesrc
-import sys, wave, struct
-from math import sqrt
+#!/usr/bin/env python3
+#coding: utf-8
+
+import sys
+import wave
+import struct
+import math
+import argparse
+
+formats = (None, '<b', '<h', '<i', '<i')
 
 def read_wav(filename):
-    waveFile = wave.open(filename, 'r')
-    data = []
+	waveFile = wave.open(filename, 'r')
+	data = []
+	t = 0
 
-    (nchannels, sampwidth, framerate, nframes, comptype, compname) = waveFile.getparams()
-    timestep = 1.0/framerate
-    print 'Framerate:',framerate
-    print 'Sample width:', sampwidth
-    t = 0
-    print 'Channels:',nchannels
-    for j in range(nframes):
-        waveData = waveFile.readframes(1)
+	(nchannels, sampwidth, framerate, nframes, comptype, compname) = waveFile.getparams()
+	timestep = 1.0 / framerate
+
+	print('Framerate: %s' % framerate)
+	print('Sample width: %s' % sampwidth)
+	print('Channels: %s' % nchannels)
+
+	assert sampwidth > 0
+
+	for j in range(nframes):
+		waveData = waveFile.readframes(1)
+
         #Take only the first channel
-        waveData = waveData[:sampwidth]
-        if sampwidth==1:
-            temp = struct.unpack_from("<b", waveData)
-        elif sampwidth==2:
-            temp = struct.unpack_from("<h", waveData)
-        elif sampwidth==3:
-            temp = struct.unpack_from("<i", waveData+'\x00')
-        elif sampwidth==4:
-            temp = struct.unpack_from("<i", waveData)
-        else:
-            raise ValueError('Unsupported sample width')
-        data.append((t*timestep,float(temp[0])/(2**(8*sampwidth)-1)))
-        t += 1
-    vrms = sqrt(1.0/(len(data[1]))*sum(i**2 for i in data[1]))
-    print 'RMS voltage:',vrms
-    return data
+		waveData = waveData[:sampwidth]
+		try:
+			waveData = waveData + bytes([0]) if sampwidth == 3 else waveData
+			fmt = formats[sampwidth]
+		except IndexError:
+			raise ValueError('Unsupported sample width')
+
+		temp = struct.unpack_from(fmt, waveData)
+		data.append((t * timestep, float(temp[0]) / (2**(8 * sampwidth) - 1)))
+		t += 1
+
+	vrms = math.sqrt(1.0 / (len(data[1])) * sum(i**2 for i in data[1]))
+	print('RMS voltage: %s' % vrms)
+	return data
 
 
-def write_spice(data,filename):
-    with open(filename,'w') as f:
-        [f.write("{:.6e} {:.4f}\n".format(*d)) for d in data]
+def write_spice(data, filename):
+	with open(filename, 'w') as f:
+		for d in data:
+			f.write("{:.6e} {:.4f}\n".format(*d))
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print """Usage: wavtospice <.wav file> <spice input file>.
-        Converts .wav to xspice filesource format."""
-        exit()
-    write_spice(read_wav(sys.argv[1]),sys.argv[2])
+	parser = argparse.ArgumentParser()
+	parser.add_argument("wav_file", help="Specify wav file as input")
+	parser.add_argument("spice_file", help="Specify output filename for spice data")
+	args = parser.parse_args()
+
+	data = read_wav(args.wav_file)
+	write_spice(data, args.spice_file)
